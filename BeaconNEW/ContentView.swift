f02e5c0aaa7ac3f833bc9c6f4e57d5a5906767a5
MagmMe <marcin@magm.me>
2020-12-15 07:49:00 +0100

    Creating project

--- BeaconNEW/ContentView.swift
+++ BeaconNEW/ContentView.swift
@@ -5,12 +5,98 @@
 //  Created by Marcin Magiera on 13/12/2020.
 //
 
+import Combine
+import CoreLocation
 import SwiftUI
 
+class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate{
+    var didChange = PassthroughSubject<Void, Never>()
+    var locationManager: CLLocationManager?
+    var lastDistance = CLProximity.unknown
+    
+    override init(){
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable(){
+                    /// skanuję w poszukiwaniu Beacona
+                    startScanning()
+                }
+                
+            }
+            
+        }
+    }
+    
+    func startScanning(){
+        
+        let uuid = UUID(uuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 0, minor: 0)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "WellCore")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if let beacon = beacons.first{
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
+    
+    func update(distance: CLProximity){
+        lastDistance = distance
+        didChange.send(())
+    }
+    
+}
+
+struct BigText: ViewModifier{
+    func body(content: Content) -> some View{
+        content
+            .font(Font.system(size: 32, design: .rounded))
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var detector = BeaconDetector()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if detector.lastDistance == .immediate{
+            return Text("Tuż obok")
+                .modifier(BigText())
+                .background(Color.red)
+                .edgesIgnoringSafeArea(.all)
+        } else if detector.lastDistance == .near {
+            return Text("Niedaleko")
+                .modifier(BigText())
+                .background(Color.orange)
+                .edgesIgnoringSafeArea(.all)
+        } else if detector.lastDistance == .far{
+            return Text("Trochę dalej")
+                .modifier(BigText())
+                .background(Color.blue)
+                .edgesIgnoringSafeArea(.all)
+        } else{
+            return Text("Nie widzę Beacona")
+                .modifier(BigText())
+                .background(Color.gray)
+                .edgesIgnoringSafeArea(.all)
+        }
+        
+            
     }
 }
 
